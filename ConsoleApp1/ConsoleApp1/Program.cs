624f65e3c2b1684622e9734ac401cec7ac745a59
人才 <123@example.com>
2026-08-03 21:19:10 +0800

    第一天

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,135 @@
+﻿using System.ComponentModel;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("Hello, World!");
+            Console.WriteLine("boy");
+            Console.WriteLine("1+2+3+4+5");
+            Console.Write("Hello, World!");
+            Console.Write("123");
+            Console.Write("c#");*/
+            //Console.Write("今天是第{0}天学习{1}", "2", "c#--");
+            //Console.Write("123");
+            //Console.WriteLine(1+2+3+4+5);
+            //Console.Write("123");
+            //Console.Write("123");
+            //int a = 1;
+            //Console.WriteLine(a);
+            //a = 6;
+            //Console.WriteLine(a);
+            //string userName = Console.ReadLine();
+            //Console.WriteLine($"名字：userName");
+            //Console.WriteLine("请输入名称");
+            //string userName =Console.ReadLine();
+            //Console.WriteLine("刚刚输入的名字是：{userName}");
+            //Console.WriteLine("请输入年龄");
+            //string userAge =Console.ReadLine();
+            //Console.WriteLine("刚刚输入的年龄是：{userAge}");
+            //string a1 = "123";
+            //string a2 = "666";
+            //string a3 = "0.5";
+            //Console.WriteLine(a1+a2+a3); 
+            //int a=int.Parse(a1);
+            //Console.WriteLine(a1);
+            //string d = "0.5";
+            //double c=double.Parse(d);
+            //Console.WriteLine(c);
+            double s = 5.0;
+            //Console.WriteLine(s/2);
+            //Console.WriteLine(s+2);
+            //Console.WriteLine(s-2);
+            //Console.WriteLine(s%2);
+            //s += s;
+            //Console.WriteLine(s);
+            //int w1 = 20;
+            //int w2 = 3;
+            //w1 += w2;
+            //Console.WriteLine(w1);//23
+            //w1 -= w2;
+            //Console.WriteLine(w1);//20
+            //w1/=w2;
+            //Console.WriteLine(w1);//6
+            //w1%=w2;
+            //Console.WriteLine(w1);//0
+            //int w3, w4;
+            //w3 = w1++;//0
+            //Console.WriteLine(w3);
+            //w4 = ++w3;//1
+            //Console.WriteLine(w4);
+            //int n = 2;
+            //int w5;
+            //w5 = n++ + ++n + ++n + ++n + n++ + 3;
+            //Console.WriteLine(w5);
+            //2+4+5+6+6+3
+            //Console.WriteLine("请输入半径：");
+            /*double r=2,C,S;
+            C = 2 * 3.14 * r;
+            S = 3.14 * r * r;
+            Console.WriteLine(C);
+            Console.WriteLine(S);*/
+            //Console.WriteLine("请输入半径：");
+            //double r=double.Parse(Console.ReadLine());
+            //double c = 2 * 3.14 * r;
+            //double S = 3.14 * r * r;
+            //Console.WriteLine($"输入的半径是：{r},周长是:{c},面积：{S}");
+
+            //Console.WriteLine("请输入第一个数字：");
+            //decimal u= decimal.Parse(Console.ReadLine());
+            //Console.WriteLine("请输入第二个数字：");
+            //decimal a = decimal.Parse(Console.ReadLine());
+            //decimal c ;
+            //c = a + u;
+            //Console.WriteLine($"数字和是:{c}");
+
+            //Console.WriteLine("请输入华氏度：");
+            //double f = double.Parse(Console.ReadLine());
+            //double h = 5 / 9.0 * (f - 32);
+            //Console.WriteLine($"华氏度：{f:F3},摄氏度：{h:F3}");
+
+            Console.WriteLine("请输入第一个值：");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("请输入第二个值：");
+            int b = int.Parse(Console.ReadLine());
+            //a = a + b;
+            //b = a - b;
+            //a = a - b;
+            int c;
+            c = a;
+            a = b;
+            b = c;
+
+            Console.WriteLine($"第一个值：{a},第二个值：{b}");
+
+            //int x = 89,n,m;
+            //n = 89 / 24;
+            //m = 89 % 24;
+            //Console.WriteLine($"共{n}天{m}小时");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
